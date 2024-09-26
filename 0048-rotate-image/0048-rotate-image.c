6e81446cc1638a40279ce99073bde567a4d11fcf
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-26 20:41:21 +0200

    Time: 3 ms (74.79%), Space: 10.1 MB (61.02%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.c
+++ 0048-rotate-image/0048-rotate-image.c
@@ -1,20 +1,17 @@
 void rotate(int** matrix, int matrixSize, int* matrixColSize) {
-    int temp;
     int i = 0;
     while(i<*matrixColSize){
         for(int j=0; j<matrixSize; j++){
-            temp = matrix[*matrixColSize-1-j][*matrixColSize-1-i];
+            int temp = matrix[*matrixColSize-1-j][*matrixColSize-1-i];
             matrix[*matrixColSize-1-j][*matrixColSize-1-i] = matrix[i][j];
             matrix[i][j] = temp;
         }
         matrixSize--;
         i++;
     }
-
-    int* temp1;
     i = 0;
     while(i<*matrixColSize/2){
-        temp1 = matrix[i];
+        int* temp1 = matrix[i];
         matrix[i] = matrix[*matrixColSize-1-i];
         matrix[*matrixColSize-1-i] = temp1;
         i++;
