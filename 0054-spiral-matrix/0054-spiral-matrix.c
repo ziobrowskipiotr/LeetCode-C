6f2c342969c2d0af0f343d75971066fc66119f42
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-26 16:55:49 +0200

    Time: 0 ms (100%), Space: 8.3 MB (30.34%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.c
+++ 0054-spiral-matrix/0054-spiral-matrix.c
@@ -1,6 +1,7 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+
 int* spiralOrder(int** matrix, int matrixSize, int* matrixColSize, int* returnSize) {
     *returnSize = (*matrixColSize) * matrixSize;
     int* result = malloc(*returnSize * sizeof(int));
