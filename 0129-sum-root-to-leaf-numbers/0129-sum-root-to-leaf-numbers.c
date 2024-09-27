8009495301c61451f96f2daf118e2ad36c7888e2
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-27 21:11:01 +0200

    Time: 0 ms (100%), Space: 8.4 MB (50.22%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.c
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.c
@@ -7,31 +7,31 @@
  * };
  */
 //0
-void dfs(struct TreeNode* root, int* tab, int i, int* sum){
+void dfs(struct TreeNode* root, int* tab, int i, int* sum, long int* numb){
     tab[i] = root->val; 
     if(root->left != NULL){
-        dfs(root->left, tab, i+1, sum);
+        dfs(root->left, tab, i+1, sum, numb);
     }
     if(root->right != NULL){
-        dfs(root->right, tab, i+1, sum);
+        dfs(root->right, tab, i+1, sum, numb);
     }
     if(root->left == NULL && root->right == NULL){
-        long int* numb = malloc(sizeof(long int));
         *numb = 1;
         for(int j=i; j>-1; j--){
             *sum += tab[j]*(*numb);
             *numb *= 10;
         }
-        free(numb);
     }
 }
 int sumNumbers(struct TreeNode* root) {
     int* tab = malloc(10*sizeof(int));
     int sum = 0;
     int* i = malloc(sizeof(int));
+    long int* numb = malloc(sizeof(long int));
     *i = 0; 
-    dfs(root, tab, *i, &sum);
+    dfs(root, tab, *i, &sum, numb);
     free(tab);
+    free(numb);
     free(i);
     return sum;
 }
