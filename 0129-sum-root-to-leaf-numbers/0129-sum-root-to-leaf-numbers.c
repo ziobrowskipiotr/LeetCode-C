7a4f86b396521042741d88eb188271f201763113
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-27 20:50:12 +0200

    Time: 0 ms (100%), Space: 8.7 MB (17.75%) - LeetHub

---
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.c
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+void dfs(struct TreeNode* root, int* tab, int i, int* sum){
+    tab[i] = root->val; 
+    if(root->left != NULL){
+        dfs(root->left, tab, i+1, sum);
+    }
+    if(root->right != NULL){
+        dfs(root->right, tab, i+1, sum);
+    }
+    if(root->left == NULL && root->right == NULL){
+        for(int j=0; j<=i; j++){
+            *sum += tab[j]*(int)pow(10, i-j);
+        }
+    }
+}
+int sumNumbers(struct TreeNode* root) {
+    int* tab = malloc(10*sizeof(int));
+    int sum = 0;
+    dfs(root, tab, 0, &sum);
+    free(tab);
+    return sum;
+}
