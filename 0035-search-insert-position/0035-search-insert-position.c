1691edbc87ec3db5fdce2d75da5dbf1f0405a1f1
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-28 21:51:09 +0200

    Time: 3 ms (66.63%), Space: 7.9 MB (97.53%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.c
+++ 0035-search-insert-position/0035-search-insert-position.c
@@ -1,27 +1,38 @@
-int searchInsert(int* nums, int numsSize, int target) {
-    int cur = numsSize/2;
-    int add = numsSize/2;
-    while(cur != 0 && cur != numsSize-1){
-        add /= 2;
-        if(nums[cur] == target){
-            return cur;
+int searchInsert(int* nums, int numsSize, int target){
+    int ip = 0;
+    int ik = numsSize-1;
+    int is = (ik+ip)/2;
+    while(ip<=ik){
+        printf("is: %d\n, ip: %d\n, ik: %d\n", is, ip, ik);
+        if(nums[is] == target){
+            printf("1, %d\n", is);
+            return is;
         }
-        else if(nums[cur-1] < target){
-            if(nums[cur+1] > target){
-                return cur;
+        else if(nums[is] > target){
+            if(is-1<ip){
+                return is;
+            }
+            else if(nums[is-1] < target){
+                printf("2, %d\n", is);
+                return is;
             }
             else{
-                cur += add;
+                ik = is - 1;
             }
         }
         else{
-            cur -= add;
+            if(is+1 > ik){
+                return is+1;
+            }
+            else if(nums[is+1] > target){
+                printf("3, %d\n", is);
+                return is+1;
+            }
+            else{
+                ip = is + 1;
+            }
         }
+        is = (ik+ip)/2;
     }
-    if(cur == 0){
-        return 0;
-    }
-    else{
-        return numsSize;
-    }
+    return -1;
 }
