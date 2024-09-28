5731838945e5bbdb9fadd76a4167eac4919a8d49
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-28 16:08:58 +0200

    Time: 7 ms (89.58%), Space: 13.4 MB (77.18%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.c
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.c
@@ -10,15 +10,15 @@ void downDfs(struct TreeNode* root, int* k, int* i, int* num){
     if(root->left != NULL){
         downDfs(root->left, k, i, num);
     }
-    if((*i)+1 < *k){
+    if((*i)+1 == *k){
+        *num = root->val;
+        (*i)++;
+    }
+    else{
         (*i)++;
         if(root->right != NULL){
             downDfs(root->right, k, i, num);
         }
-    }
-    else if((*i)+1 == *k){
-        *num = root->val;
-        (*i)++;
     }
 }
 int kthSmallest(struct TreeNode* root, int k) {
