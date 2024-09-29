a30a215f694206f0b9e8acf9ceeb274027d63fc6
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-29 17:06:06 +0200

    Time: 0 ms (100%), Space: 8.6 MB (83.16%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.c
@@ -0,0 +1,32 @@
+bool searchMatrix(int** matrix, int matrixSize, int* matrixColSize, int target) {
+    int i = 0;
+    int jp = 0;
+    int jk = matrixColSize[0]-1;
+    int js;
+    while(i < matrixSize){
+        if(matrix[i][0] <= target){
+            if(matrix[i][matrixColSize[i]-1] >= target){
+                while(jp<=jk){
+                    js = (jp+jk)/2;
+                    if(matrix[i][js] == target){
+                        return true;
+                    }
+                    else if(matrix[i][js] > target){
+                        jk = js-1;
+                    }
+                    else{
+                        jp = js+1;
+                    }
+                }
+                return false;
+            }
+            else{
+                i++;
+            }
+        }
+        else{
+            return false;
+        }
+    }
+    return false;        
+}
