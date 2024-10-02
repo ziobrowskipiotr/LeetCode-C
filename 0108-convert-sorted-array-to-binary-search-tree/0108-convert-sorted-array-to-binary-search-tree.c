20ebeae6a2b892a46e2ca7c6a3efbbf7b6d05a7c
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-10-02 15:48:20 +0200

    Time: 8 ms (42.11%), Space: 10.6 MB (99.85%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.c
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.c
@@ -6,33 +6,32 @@
  *     struct TreeNode *right;
  * };
  */
-void left(int actual, int ip, int ik, int* nums, struct TreeNode* tab){
+void left(int actual, int ip, int ik, struct TreeNode* tab){
     if(ip<=ik){
         int is = (ip+ik)/2;
         tab[actual].left = &tab[is];
-        left(is, ip, is-1, nums, tab);
-        right(is, is+1, ik, nums, tab);
+        left(is, ip, is-1, tab);
+        right(is, is+1, ik, tab);
     }
 }
-void right(int actual, int ip, int ik, int* nums, struct TreeNode* tab){
+void right(int actual, int ip, int ik, struct TreeNode* tab){
     if(ip<=ik){
         int is = (ip+ik)/2;
         tab[actual].right = &tab[is];
-        left(is, ip, is-1, nums, tab);
-        right(is, is+1, ik, nums, tab);
+        left(is, ip, is-1, tab);
+        right(is, is+1, ik, tab);
     }
 }
 struct TreeNode* sortedArrayToBST(int* nums, int numsSize) {
 
     struct TreeNode* tab = malloc(numsSize * sizeof(struct TreeNode));
-
     for(int i=0; i<numsSize; i++){
         tab[i].val = nums[i];
         tab[i].left = NULL;
         tab[i].right = NULL;
     }
     int is = (numsSize-1)/2;
-    left(is, 0, is-1, nums, tab);
-    right(is, is+1, numsSize-1, nums, tab);
+    left(is, 0, is-1, tab);
+    right(is, is+1, numsSize-1, tab);
     return &tab[is];
 }
