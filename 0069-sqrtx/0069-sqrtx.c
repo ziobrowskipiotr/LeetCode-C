f89af0c67c1cacb9033bb7e21fc3e558b998fbbc
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-10-03 15:14:00 +0200

    Time: 0 ms (100%), Space: 8.5 MB (40.99%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.c
+++ 0069-sqrtx/0069-sqrtx.c
@@ -1,14 +1,3 @@
 int mySqrt(int x) {
-    int i = 0;
-    unsigned int* tab = malloc(3*sizeof(unsigned int));
-    tab[0] = 0;
-    tab[1] = 1;
-    tab[2] = 2;
-    while(tab[0]<=x){
-        tab[0] = tab[0]+tab[1];
-        tab[1] = tab[1] + tab[2];
-        i = i+1;
-    }
-    free(tab);
-    return i-1;
+    return sqrt(x);
 }
