256f0835d45de6857a99703086f2a9f1caa05a2f
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-10-03 14:42:09 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.c
@@ -0,0 +1,12 @@
+int mySqrt(int x) {
+    int i = 0;
+    int a = 0;
+    int r1 = 1;
+    int r2 = 2;
+    while(a<=x){
+        a = a+r1;
+        r1 = r1 + r2;
+        i = i+1;
+    }
+    return i-1;
+}
