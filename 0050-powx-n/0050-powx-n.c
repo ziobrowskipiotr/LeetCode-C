a3de35f6ec03a66067f0c06abeb4c14a4d2ba825
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-10-04 17:44:48 +0200

    Time: 2 ms (57.48%), Space: 7.9 MB (70.4%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.c
@@ -0,0 +1,34 @@
+double pos_Pow(double ans, double x, unsigned int n, int med){
+    if(n < 2){
+        return x;
+    }
+    med = n/2;
+    ans = pos_Pow(ans, x, med, med);
+    if(n%2 == 1){
+        ans *= ans*(x);
+        return ans;
+    }
+    else{
+        ans *= ans;
+        return ans;
+    }
+}
+
+double myPow(double x, int n) {
+    if(n == 0 || x == 1){
+        return 1;
+    }
+    double ans = 1;
+    int med;
+    unsigned int exp;
+    if(n > 0){
+        exp = n;
+        return pos_Pow(ans, x, exp, med);
+    }
+    else{
+        n++;
+        exp = -n;
+        exp++;
+        return 1/pos_Pow(ans, x, exp, med);
+    }
+}
