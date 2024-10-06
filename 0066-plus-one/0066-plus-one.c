e3f49501b58d9ae27bc690b9a9252da2fdb7dff7
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-10-06 19:44:43 +0200

    Time: 5 ms (34.67%), Space: 9.3 MB (50.06%) - LeetHub

--- 0066-plus-one/0066-plus-one.c
+++ 0066-plus-one/0066-plus-one.c
@@ -1,11 +1,10 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-//1
-int* change(int* digits, int* digitsSize, int* returnSize){
-    if(digits[*digitsSize] == 9){
-        digits[*digitsSize] = 0;
-        if(*digitsSize == 0){
+int* change(int* digits, int actual, int* returnSize){
+    if(digits[actual] == 9){
+        digits[actual] = 0;
+        if(actual == 0){
             (*returnSize)++;
             int* digits_new = malloc(*returnSize*sizeof(int));
             digits_new[0] = 1;
@@ -13,16 +12,14 @@ int* change(int* digits, int* digitsSize, int* returnSize){
             free(digits);
             return digits_new;
         }
-        else{
-            (*digitsSize)--;
-            return change(digits, digitsSize, returnSize);
+        else{        
+            return change(digits, actual-1, returnSize);
         }
     }
-    digits[*digitsSize]++;
+    digits[actual]++;
     return digits;
 }
 int* plusOne(int* digits, int digitsSize, int* returnSize) {
     *returnSize = digitsSize;
-    digitsSize--;
-    return change(digits, &digitsSize, returnSize);
+    return change(digits, digitsSize-1, returnSize);
 }
